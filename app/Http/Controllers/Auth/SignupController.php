c9aa22bbadce73308477a55cfe34dceb309b60a9
cuong <chicuongnguyenit@gmail.com>
2016-10-28 23:33:27 +0700

    Signup error

--- app/Http/Controllers/Auth/SignupController.php
+++ app/Http/Controllers/Auth/SignupController.php
@@ -39,13 +39,15 @@ class SignupController extends Controller
             if($request->isMethod('post'))
             {
                 $results = $authService->signup($name, $email, $phone, $password, $confirmPassword);
-                $user = $results['data'];
-
-                if($roleLink != KACANA_AUTH_ADMIN_NAME)
-                    \Auth::loginUsingId($user->id, true);
 
                 if($results['ok'])
+                {
+                    $user = $results['data'];
+                    if($roleLink != KACANA_AUTH_ADMIN_NAME)
+                        \Auth::loginUsingId($user->id, true);
                     $emailService->sendEmailNewUser($email);
+                }
+
 
                 if($postAjax)
                     return response()->json($results);
