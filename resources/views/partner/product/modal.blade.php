a9c1aba6007d70cd51a0e24599cfe9d24c26c839
cuong <chicuongnguyenit@gmail.com>
2017-04-10 22:28:38 +0700

    improvement partner boot layout

--- resources/views/partner/product/modal.blade.php
+++ resources/views/partner/product/modal.blade.php
@@ -1,33 +1,60 @@
 <div id="modal-supper-boot-product" class="modal fade" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                <h4 class="modal-title" id="myModalLabel">Super boot sản </h4>
-            </div>
             <div class="modal-body">
-                <label class="vpadding-10 hpadding-10 text-center text-red title-index" >Chọn hình sản phẩm</label>
-                <div class="list-product-super-boot-item" >
+                <div class="nav-tabs-custom">
+                    <ul class="nav nav-tabs pull-right">
+                        <li class=""><a class="text-red" href="#tab_1-1" data-toggle="tab" aria-expanded="false">Chọn tài khoản cần đăng</a></li>
+                        <li class=""><a class="text-red" href="#tab_2-2" data-toggle="tab" aria-expanded="false">Mô tả bài đăng</a></li>
+                        <li class="active"><a class="text-red" href="#tab_3-2" data-toggle="tab" aria-expanded="true">Chọn hình sản phẩm</a></li>
+                        {{--<li class="dropdown">--}}
+                            {{--<a class="dropdown-toggle" data-toggle="dropdown" href="#">--}}
+                                {{--Dropdown <span class="caret"></span>--}}
+                            {{--</a>--}}
+                            {{--<ul class="dropdown-menu">--}}
+                                {{--<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>--}}
+                                {{--<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>--}}
+                                {{--<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>--}}
+                                {{--<li role="presentation" class="divider"></li>--}}
+                                {{--<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>--}}
+                            {{--</ul>--}}
+                        {{--</li>--}}
+                        <li class="pull-left header"><i class="fa fa-rocket"></i> Super boot sản</li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane" id="tab_1-1">
+                            <div class="list-social-post">
+                                @if(count($facebookAccountBusiness))
+                                    <ul class="social-list clearfix">
+                                        @foreach($facebookAccountBusiness as $item)
+                                            <li data-social-id="{{$item->social_id}}" data-type="{{$item->type}}" class="active">
+                                                <img src="{{$item->image}}">
+                                                <a class="social-name" href="#">{{$item->name}}</a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                @else
+                                    <h4> Không có tài khoản facebook! vui lòng thêm tài khoản facebook <a href="/social_account">tại đây</a></h4>
+                                @endif
+                            </div>
+                        </div>
 
-                </div>
-                <label class="vpadding-10 hpadding-10 text-center text-red title-index" >Mô tả bài đăng</label>
-                <div contenteditable="true" class="desc-post-to-social" >
+                        <!-- /.tab-pane -->
+                        <div class="tab-pane" id="tab_2-2">
+                            <div contenteditable="true" class="desc-post-to-social" >
+                            </div>
+                        </div>
+                        <!-- /.tab-pane -->
+                        <div class="tab-pane active" id="tab_3-2">
+                            <div class="list-product-super-boot-item" >
+
+                            </div>
+                        </div>
+                        <!-- /.tab-pane -->
                     </div>
-                <label class="vpadding-10 hpadding-10 text-center text-red title-index" >Chọn tài khoản cần đăng</label>
-                <div class="list-social-post">
-                    @if(count($facebookAccountBusiness))
-                        <ul class="social-list clearfix">
-                            @foreach($facebookAccountBusiness as $item)
-                                <li data-social-id="{{$item->social_id}}" data-type="{{$item->type}}" class="active">
-                                    <img src="{{$item->image}}">
-                                    <a class="social-name" href="#">{{$item->name}}</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    @else
-                      <h4> Không có tài khoản facebook! vui lòng thêm tài khoản facebook <a href="/social_account">tại đây</a></h4>
-                    @endif
+                    <!-- /.tab-content -->
                 </div>
+
             </div>
             <div class="modal-footer">
                 @if(count($facebookAccountBusiness))
@@ -37,6 +64,9 @@
             </div>
         </div>
     </div>
+</div>
+<div>
+
 </div>
 
 <script id="template-desc-super-post-to-social" type="template">
