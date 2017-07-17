4108fd3a4aa9138f8cf63e963f3e94d3ba56fe3a
cuong.nguyen <cuong.nguyen@zinio.com>
2017-07-18 00:13:38 +0700

    add function for auto backup db and upload to s3

--- scriptCommand/backup_db.sh
+++ scriptCommand/backup_db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do php artisan db:backup --upload-s3 kacanabucket; sleep 5m; done
+while true; do php artisan db:backup --keep-only-s3 --upload-s3 kacanabucket; sleep 5m; done
