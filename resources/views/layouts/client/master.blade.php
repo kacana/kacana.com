f93ae4699f1f9110547eeb369e36284eb16d2cad
cuong.nguyen <cuong.nguyen@zinio.com>
2018-06-21 14:56:48 +0700

    fix remarketing for social

--- resources/views/layouts/client/master.blade.php
+++ resources/views/layouts/client/master.blade.php
@@ -183,7 +183,6 @@
         gtag('js', new Date());
 
         gtag('config', 'UA-82621615-2');
-        gtag('config', '938027284');
 
     </script>
 </head>
@@ -299,7 +298,15 @@
     gtag('event', 'page_view', google_tag_params);
 
 </script>
-
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 938027284;
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
 <noscript>
     <div style="display:inline;">
         <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/938027284/?guid=ON&amp;script=0"/>
